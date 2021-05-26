93a09cc3b54d3c9536ca36854f40bc6caed7b84f
harshrajput1506 <harshrajput2906@gmail.com>
2021-05-26 15:33:50 +0530

    topics

--- hyperquizo/api/quiz/quizcategory.js
+++ hyperquizo/api/quiz/quizcategory.js
@@ -1,27 +1,40 @@
 const mysql = require("../../config/database")
-const { getQuizContests } = require("./contest");
-const { getTrendingTopics } = require("./quiz.service");
 module.exports = {
     getQuizCategory: (callback) => {
-        mysql.query('select * from topics order by title', [], (error, results, fields) => {
+        const topTopics;
+        const trendingTopics;
+        const allTopics;
+        mysql.query('select topicId, title, icon from topics order by title', [], (error, results, fields) => {
             if (error) {
                 callback(error);
             }
-            const data = results;  //Array
-            getTrendingTopics((err, results) => {
-                if(err){
-                    callback(err)
-                }
-                const trendingTopics = results;  // Trending Array
-                const topicsData = {
-                    topics: data,
-                    trendingTopics: trendingTopics
-                };
-
-                return callback(null, topicsData);
-            });
+            allTopics = results;  //Array List All Topics
             
         });
+
+        mysql.query('select topicId, title, icon from topics order by views desc limit 8', [], (error, results, fields) => {
+            if (error) {
+                callback(error);
+            }
+            topTopics = results;  //Array List Top Topics
+            
+        });
+
+        mysql.query('select topicId, title, icon from topics where isTrending = 1 order by views desc limit 4', [], (error, results, fields) => {
+            if (error) {
+                callback(error);
+            }
+            trendingTopics = results;  //Array List Trending Topics
+            
+        });
+
+        const data = {
+            allTopics: allTopics,
+            topTopics: topTopics,
+            trendingTopics: trendingTopics
+        }
+
+        return callback(null, data);
     },
 
 };

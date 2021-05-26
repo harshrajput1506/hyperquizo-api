6f4820c2a46d29903c0e41ffc311ed5fb9e20a61
harshrajput1506 <harshrajput2906@gmail.com>
2021-05-26 15:46:57 +0530

    topics

--- hyperquizo/api/quiz/quiz.service.js
+++ hyperquizo/api/quiz/quiz.service.js
@@ -1,7 +1,21 @@
 const mysql = require("../../config/database");
 module.exports = {
     getTrendingTopics: (callback) => {
-        mysql.query("select * from topics order by views desc limit 8", [], (error, results, fields) =>{
+        mysql.query("select topicId, title, icon from topics where isTrending = 1 order by views desc limit 4", [], (error, results, fields) =>{
+            if (error) {
+                callback(error);
+            }
+            if(results){
+                return callback(null, results);
+            } else {
+                return callback("Fetching Error", null);
+            }
+
+        });
+    },
+
+    getTopTopics: (callback) => {
+        mysql.query("select topicId, title, icon from topics order by views desc limit 8", [], (error, results, fields) =>{
             if (error) {
                 callback(error);
             }
--- hyperquizo/api/quiz/quizcategory.js
+++ hyperquizo/api/quiz/quizcategory.js
@@ -1,24 +1,33 @@
 const mysql = require("../../config/database")
 const { getQuizContests } = require("./contest");
-const { getTrendingTopics } = require("./quiz.service");
+const { getTrendingTopics, getTopTopics } = require("./quiz.service");
 module.exports = {
     getQuizCategory: (callback) => {
-        mysql.query('select * from topics order by title', [], (error, results, fields) => {
+        mysql.query('select topicId, title, icon from topics order by title', [], (error, results, fields) => {
             if (error) {
                 callback(error);
             }
-            const data = results;  //Array
+            const allTopics = results;  //Array
             getTrendingTopics((err, results) => {
                 if(err){
                     callback(err)
                 }
                 const trendingTopics = results;  // Trending Array
-                const topicsData = {
-                    topics: data,
-                    trendingTopics: trendingTopics
-                };
+                getTopTopics((err, results) => {
+                    if(err){
+                        callback(err)
+                    }
 
-                return callback(null, topicsData);
+                    const topTopics = results;   // Top Topics Array
+
+                    const topicsData = {
+                        topics: allTopics,
+                        trendingTopics: trendingTopics,
+                        topTopics: topTopics
+                    };
+                    return callback(null, topicsData);
+                });
+
             });
             
         });
